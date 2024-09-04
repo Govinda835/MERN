6088f09f3f984c30980973e6c4b43a3890b1a475
Govinda835 <chaudharygovinda835@gmail.com>
2024-09-05 01:11:25 +0545

    api and port number

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require("express")
 const app = express()
 
+app.get("/",(req,res)=>{
+    res.json({
+        message: "This is the home page...!!!"
+    })
+})
+
+app.get("/about",(req,res)=>{
+    res.json({
+        message: "This is the about page...!!!"
+    })
+})
 
 app.listen(3000, ()=>{
     console.log("nodeJs project has been started....!!!")
