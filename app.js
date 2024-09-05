7931d5c01ba691e7db7d984e7c7602e1af701208
Govinda835 <chaudharygovinda835@gmail.com>
2024-09-05 18:40:30 +0545

    orm, odm and database connection mongodb

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ const express = require("express")
 const app = express()
 
 app.get("/",(req,res)=>{
-    res.json({
-        message: "This is the home page...!!!"
+    res.status(200).json({
+        hello : "This is the home page...!!!"
     })
 })
 
 app.get("/about",(req,res)=>{
-    res.json({
+    res.status(200).json({
         message: "This is the about page...!!!"
     })
 })
 
 app.listen(3000, ()=>{
-    console.log("nodeJs project has been started....!!!")
+    console.log("nodeJs project has started....!!!")
 })
